8510e028b103dbcb58fd9c7790cda4112e7c6939
piccolojnr <rahimdaud246@gmail.com>
2024-01-03 04:13:12 +0000

    perfect tree betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,12 +9,13 @@
 size_t depth(const binary_tree_t *tree)
 {
 	size_t d = 0;
+
 	while (tree != NULL)
 	{
 		d++;
 		tree = tree->left;
 	}
-	return d;
+	return (d);
 }
 
 /**
@@ -23,33 +24,32 @@ size_t depth(const binary_tree_t *tree)
  * @d: depth
  * @level: level
  *
- * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0.
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return (0).
  */
 int is_perfect(const binary_tree_t *tree, int d, int level)
 {
-	// Check if the tree is empty
 	if (tree == NULL)
-		return 1;
+		return (1);
 
-	// Check the presence of children
 	if (tree->left == NULL && tree->right == NULL)
 		return (d == level + 1);
 
 	if (tree->left == NULL || tree->right == NULL)
-		return 0;
+		return (0);
 
-	return is_perfect(tree->left, d, level + 1) &&
-		   is_perfect(tree->right, d, level + 1);
+	return (is_perfect(tree->left, d, level + 1)
+	&& is_perfect(tree->right, d, level + 1));
 }
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  *
- * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0.
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return (0).
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int d = depth(tree);
-	return is_perfect(tree, d, 0);
+
+	return (is_perfect(tree, d, 0));
 }
